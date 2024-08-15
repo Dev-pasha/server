02813454851955d761a47d38c04449c89381d6b6
Dev-pasha <sp19-bcs-141@cuilahore.edu.pk>
2024-08-15 13:25:20 +0500

    files added

---
+++ index.js
@@ -0,0 +1,27 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import dotenv from 'dotenv';
+import { connectDB } from './config.js';
+import productRoutes from './routes/productRoutes.js';
+
+dotenv.config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+connectDB();
+
+
+app.use('/api/products', productRoutes);
+app.get('/', (req, res) => {
+  res.send('API is running...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
